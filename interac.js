3ffedb92377ac06bcd4fba748b7943b7e06e66bc
FFFiend <96851409+FFFiend@users.noreply.github.com>
2021-12-30 14:23:43 +0300

    changed file name and variable name

---
+++ interac.js
@@ -0,0 +1,7 @@
+let b = new Date();
+if (b.getDay() % 2 == 0){
+    alert('Today is an even day')
+}
+else{
+    alert('Today is not an even day')
+}
--- k.js
+++
@@ -1,8 +0,0 @@
-let k = new Date();
-if (k.getDay() % 2 == 0){
-    alert('Today is a cool day')
-}
-else{
-    alert('Today is not a cool day')
-}
-alert("The date is " + k);
