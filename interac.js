786d420d641111b8c8ebc33320f468335a411e0b
FFFiend <96851409+FFFiend@users.noreply.github.com>
2021-12-30 20:46:51 +0300

    Update interac.js

--- interac.js
+++ interac.js
@@ -3,6 +3,9 @@ let b = 6 - f.getDay();
 if (b > 1){
     alert('Only ' + b + ' days to the weekend! :D')
 }
-else {
+else if (b == 1) {
     alert('Only ' + b + ' day to the weekend! :D')
+}
+else {
+    alert('It"\'"s the weekend!' )
 }
